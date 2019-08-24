b2bc2077157198bc6b5f0adb27702d82d4f99701
tiago154 <tiago.toya@hotmail.com>
2019-08-24 18:32:07 -0300

    Implementando a demonstração

--- __tests__/unit/demo/index.test.js
+++ __tests__/unit/demo/index.test.js
@@ -1,3 +1,91 @@
-const calcularMedia = require('../../../demo');
+const { calcularMedia, respostas } = require('../../../demo');
 
+describe('Valores válidos', () => {
+    it('A mensagem deve retornar Aprovado com Distinção caso a média seja 10', () => {
+        const resultado = calcularMedia(10, 10);
 
+        expect(resultado).toBe(respostas.aprovadoDisticao);
+    });
+
+    it('A mensagem deve retornar Aprovado caso a média seja igual ou maior que 7 e menor 10', () => {
+        const resultado = calcularMedia(10, 9);
+
+        expect(resultado).toBe(respostas.aprovado);
+    });
+
+    it('A mensagem deve retornar Reprovado caso a média seja menor 7', () => {
+        const resultado = calcularMedia(7, 6);
+
+        expect(resultado).toBe(respostas.reprovado);
+    });
+});
+
+describe('Valores inválidos', () => {
+    describe('Primeiro número', () => {
+        describe('Não é um número', () => {
+            const valores = [
+                { primeiraNota: null, segundaNota: 10 },
+                { primeiraNota: undefined, segundaNota: 10 },
+                { primeiraNota: '10', segundaNota: 10 },
+                { primeiraNota: {}, segundaNota: 10 }
+            ];
+
+            valores.forEach(({ primeiraNota, segundaNota }) => {
+                it(`Primeira Nota: ${primeiraNota}`, () => {
+                    const resultado = calcularMedia(primeiraNota, segundaNota);
+
+                    expect(resultado).toBe(respostas.erro);
+                })
+            });
+        });
+
+        describe('Nota inválida', () => {
+            const valores = [
+                { primeiraNota: 11, segundaNota: 10 },
+                { primeiraNota: -2, segundaNota: 10 },
+            ];
+
+            valores.forEach(({ primeiraNota, segundaNota }) => {
+                it(`Primeira Nota: ${primeiraNota}`, () => {
+                    const resultado = calcularMedia(primeiraNota, segundaNota);
+
+                    expect(resultado).toBe(respostas.erro);
+                })
+            });
+        });
+    });
+
+    describe('Segundo número', () => {
+        describe('Não é um número', () => {
+            const valores = [
+                { primeiraNota: 10, segundaNota: null },
+                { primeiraNota: 9, segundaNota: undefined },
+                { primeiraNota: 8, segundaNota: '10' },
+                { primeiraNota: 7, segundaNota: {} }
+            ];
+
+            valores.forEach(({ primeiraNota, segundaNota }) => {
+                it(`Segunda Nota: ${segundaNota}`, () => {
+                    const resultado = calcularMedia(primeiraNota, segundaNota);
+
+                    expect(resultado).toBe(respostas.erro);
+                })
+            });
+        });
+
+        describe('Nota inválida', () => {
+            const valores = [
+                { primeiraNota: 8, segundaNota: 11 },
+                { primeiraNota: 7, segundaNota: -5 },
+            ];
+
+            valores.forEach(({ primeiraNota, segundaNota }) => {
+                it(`Segunda Nota: ${segundaNota}`, () => {
+                    const resultado = calcularMedia(primeiraNota, segundaNota);
+
+                    expect(resultado).toBe(respostas.erro);
+                })
+            });
+        });
+    });
+});
--- demo/index.js
+++ demo/index.js
@@ -3,10 +3,35 @@
 // A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 // A mensagem "Reprovado", se a média for menor do que sete;
 // A mensagem "Aprovado com Distinção", se a média for igual a dez.
-// Em caso de qualquer erro no calculo apresentar a mensagem "Informações Inválidas";
+// Valores que não sejam numeros ou notas validas deverá retornar a mensagem "Informações Inválidas";
+
+const respostas = {
+    aprovadoDisticao: 'Aprovado com Distinção',
+    aprovado: 'Aprovado',
+    erro: 'Informações Inválidas',
+    reprovado: 'Reprovado',
+};
+
+const calcularMedia = (primeiraNota, segundaNota) => {
+    if (typeof (primeiraNota) !== 'number' || typeof (segundaNota) !== 'number')
+        return respostas.erro;
+
+    if ((primeiraNota > 10 || primeiraNota < 0) || (segundaNota > 10 || segundaNota < 0))
+        return respostas.erro;
+
+    const media = (primeiraNota + segundaNota) / 2;
+
+    if (media === 10) {
+        return respostas.aprovadoDisticao;
+    } else if (media >= 7 && media < 10) {
+        return respostas.aprovado;
+    } else {
+        return respostas.reprovado;
+    }
+};
 
 
-const calcularMedia = (primeiraNota, segundaNota) => null;
-
-
-module.exports = calcularMedia;
+module.exports = {
+    calcularMedia,
+    respostas
+};
